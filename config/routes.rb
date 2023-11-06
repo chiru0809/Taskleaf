b0b63c36ed35368412e310556ac97fd1469dcc6b
EC2 Default User <ec2-user@ip-172-31-17-60.ap-northeast-1.compute.internal>
2023-11-06 12:34:25 +0000

    [Add]login function

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
-  
+
   root to: 'tasks#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logion', to: 'sessions#destroy'
   resources :tasks
 end
